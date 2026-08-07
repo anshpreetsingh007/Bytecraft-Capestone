54173d31273bfbd71076b282eebe62dd5e1bd788
anshpreetsingh007 <anshpreet.singh@edu.sait.ca>
2026-08-07 15:34:04 -0600

    db fix

--- microservices/estimate-service/src/config/db.ts
+++ microservices/estimate-service/src/config/db.ts
@@ -6,21 +6,12 @@ if (!process.env.DB_HOST) {
 }
 
 const pool = new Pool({
-<<<<<<< HEAD
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432'),
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-=======
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432"),
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
->>>>>>> 3e0b845ed8acde0e3687e27b7ccc8651997612b5
 });
 
 export default pool;
